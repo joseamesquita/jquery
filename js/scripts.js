541c415bef82b61dfa422cd30c15dacd6be1764e
Jose Amesquita and Jamison Cozart <jamison@>
2020-01-13 14:07:23 -0800

    1. add function to take the middle element and insert it to the beginning of the prompt

--- js/scripts.js
+++ js/scripts.js
@@ -10,19 +10,29 @@ var uppercaseFirstLast = function(sentence) {
       newSentence += (sentence[i]);
     }
   }
-  return newSentence;
+  var firstLast = newSentence[0] + newSentence[sentence.length - 1];
+  return firstLast;
 }
-
-console.log(uppercaseFirstLast(sentence));
-
-
-
 
 var switchFirstLast = function(sentence) {
   var result = "";
   result += sentence[sentence.length - 1]
   result += sentence.slice(1, sentence.length - 1)
   result += sentence[0]
-  return result;
+  var lastFirst = result[0] + result[result.length - 1];
+  return lastFirst;
 }
-console.log(switchFirstLast(uppercaseFirstLast(sentence)));
+
+var cipher = function(sentence) {
+  return sentence += switchFirstLast(uppercaseFirstLast(sentence));
+}
+
+var midFirst = function(sentence){
+  var mid = Math.floor(sentence.length / 2);
+  var result = cipher(sentence);
+  var final = sentence[mid] + result;
+  return final
+  
+}
+
+console.log(midFirst(sentence));
